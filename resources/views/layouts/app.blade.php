3fe560a7cbfdc2df8ba01f38def1d11000701a60
kyawthura-gg <ggaryal@gmail.com>
2021-07-18 21:05:28 +0630

    feat: add livewire in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
+    <livewire:styles />
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
@@ -107,6 +108,8 @@
             </div>
         </div>
     </main>
+
+    <livewire:scripts />
 </body>
 
 </html>
