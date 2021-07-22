a8c1310585f2a6013bf517462ecdb4080fb9e46e
kyawthura-gg <ggaryal@gmail.com>
2021-07-22 17:14:58 +0630

    fix: avator

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,21 @@ class User extends Authenticatable
     {
         return $this->hasMany(Idea::class);
     }
+
+    public function getAvatar()
+    {
+        $firstCha = $this->email[0];
+
+        $emailToInteger = is_numeric($firstCha)
+            ? ord(strtolower($firstCha)) - 21
+            : ord(strtolower($firstCha)) - 96;
+
+
+        return 'https://www.gravatar.com/avatar/'
+            . md5($this->email)
+            . '?s=200'
+            . '&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-'
+            . $emailToInteger
+            . '.png';
+    }
 }
