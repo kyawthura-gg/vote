044125de8fca56a74849e5e1a3c3772348969dc0
kyawthura-gg <ggaryal@gmail.com>
2021-07-22 17:15:13 +0630

    feat: avator test case

---
+++ tests/Unit/AvatorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AvatorTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function avator_generate_by_email_first_charactor()
+    {
+        $user = User::factory()->create([
+            'name' => 'Andre',
+            'email' => 'afakeemail@fakeemail.com',
+        ]);
+
+        $gravatarUrl = $user->getAvatar();
+
+        $this->assertEquals(
+            'https://www.gravatar.com/avatar/' . md5($user->email) . '?s=200&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-1.png',
+            $gravatarUrl
+        );
+    }
+}
