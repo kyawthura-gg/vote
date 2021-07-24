fa5397ed9906011b1559386f28bdb22a00a8dcb8
kyawthura-gg <ggaryal@gmail.com>
2021-07-24 16:16:08 +0630

    fix: moved form to livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,35 +59,29 @@
                      ">
                 <div class="px-6 py-2 pt-6 text-center">
                     <h3 class="text-base font-semibold">Add an idea</h3>
-                    <p class="mt-4 text-xs">Let us know what you like and we'll take a look over!</p>
+
+                    <p class="mt-4 text-xs">
+                        @auth
+                        Let us know what you would like and we'll take a look over!
+                        @else
+                        Please login to create an idea.
+                        @endauth
+                    </p>
                 </div>
-                <form action="#" method="POST" class="px-4 py-6 space-y-4">
-                    <div>
-                        <input type="text" class="w-full px-4 py-2 text-sm placeholder-gray-900 bg-gray-100 border-none rounded-xl" placeholder="Your Idea">
-                    </div>
-                    <div>
-                        <select name="category_add" id="category_add" class="w-full px-4 py-2 text-sm bg-gray-100 border-none rounded-xl">
-                            <option value="Category One">Category One</option>
-                            <option value="Category Two">Category Two</option>
-                            <option value="Category Three">Category Three</option>
-                            <option value="Category Four">Category Four</option>
-                        </select>
-                    </div>
-                    <div>
-                        <textarea name="idea" id="idea" cols="30" rows="4" class="w-full px-4 py-2 text-sm placeholder-gray-900 bg-gray-100 border-none rounded-xl" placeholder="Describe your idea"></textarea>
-                    </div>
-                    <div class="flex items-center justify-between space-x-3">
-                        <button type="button" class="flex items-center justify-center w-1/2 px-6 py-3 text-xs font-semibold transition duration-150 ease-in bg-gray-200 border border-gray-200 h-11 rounded-xl hover:border-gray-400">
-                            <svg class="w-4 text-gray-600 transform -rotate-45" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
-                            </svg>
-                            <span class="ml-1">Attach</span>
-                        </button>
-                        <button type="submit" class="flex items-center justify-center w-1/2 px-6 py-3 text-xs font-semibold text-white transition duration-150 ease-in border h-11 bg-blue rounded-xl border-blue hover:bg-blue-hover">
-                            Submit
-                        </button>
-                    </div>
-                </form>
+
+                @auth
+                <livewire:create-idea />
+                @else
+                <div class="my-8 text-center">
+                    <a href="{{ route('login') }}" class="justify-center inline-block w-1/2 px-6 py-3 text-xs font-semibold text-white transition duration-150 ease-in border h-11 bg-blue rounded-xl border-blue hover:bg-blue-hover">
+                        Login
+                    </a>
+                    <a href="{{ route('register') }}" class="justify-center inline-block w-1/2 px-6 py-3 mt-4 text-xs font-semibold transition duration-150 ease-in bg-gray-200 border border-gray-200 h-11 rounded-xl hover:border-gray-400">
+                        Sign Up
+                    </a>
+                </div>
+                @endauth
+
             </div>
         </div>
         <div class="w-full px-2 md:w-175 md:px-0">
