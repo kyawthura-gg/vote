33192dba481406c43202d30c5c1abea9a73548d3
kyawthura-gg <ggaryal@gmail.com>
2021-07-24 16:16:25 +0630

    feat: added test cases for create idea

---
+++ tests/Feature/CreateIdeaTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class CreateIdeaTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function create_idea_form_does_not_show_when_logged_out()
+    {
+        $response = $this->get(route('idea.index'));
+
+        $response->assertSuccessful();
+        $response->assertSee('Please login to create an idea.');
+    }
+
+    /** @test */
+    public function create_idea_form_does_show_when_logged_in()
+    {
+        $response = $this->actingAs(User::factory()->create())->get(route('idea.index'));
+
+        $response->assertSuccessful();
+        $response->assertSee('Let us know what you would like and we\'ll take a look over!', false);
+    }
+
+    /** @test */
+    public function form_validation_works_for_create_idea_form()
+    {
+        Livewire::actingAs(User::factory()->create())
+            ->test(CreateIdea::class)
+            ->set('title', '')
+            ->set('category', '')
+            ->set('description', '')
+            ->call('createIdea')
+            ->assertHasErrors(['title', 'category', 'description'])
+    }
+
+    /** @test */
+    public function creating_idea_works()
+    {
+        $user = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+        $categoryTwo = Category::factory()->create(['name' => 'Category 2']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        Livewire::actingAs($user)
+            ->test(CreateIdea::class)
+            ->set('title', 'My First Idea')
+            ->set('category', $categoryOne->id)
+            ->set('description', 'This is my first idea')
+            ->call('createIdea')
+            ->assertRedirect('/');
+
+        $response = $this->actingAs($user)->get(route('idea.index'));
+        $response->assertSuccessful();
+        $response->assertSee('My First Idea');
+        $response->assertSee('This is my first idea');
+    }
+
+    /** @test */
+    public function creating_two_ideas_has_different_slugs()
+    {
+        $user = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+        $categoryTwo = Category::factory()->create(['name' => 'Category 2']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        Livewire::actingAs($user)
+            ->test(CreateIdea::class)
+            ->set('title', 'My First Idea')
+            ->set('category', $categoryOne->id)
+            ->set('description', 'This is my first idea')
+            ->call('createIdea')
+            ->assertRedirect('/');
+
+        $this->assertDatabaseHas('ideas', [
+            'title' => 'My First Idea',
+            'slug' => 'my-first-idea'
+        ]);
+
+        Livewire::actingAs($user)
+            ->test(CreateIdea::class)
+            ->set('title', 'My First Idea')
+            ->set('category', $categoryOne->id)
+            ->set('description', 'This is my first idea')
+            ->call('createIdea')
+            ->assertRedirect('/');
+
+        $this->assertDatabaseHas('ideas', [
+            'title' => 'My First Idea',
+            'slug' => 'my-first-idea-1'
+        ]);
+    }
+}
