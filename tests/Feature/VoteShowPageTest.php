0fa1ccdfa3d5bcdcb5b6ac30cd1c635710f9fb42
kyawthura-gg <ggaryal@gmail.com>
2021-07-26 17:21:32 +0630

    feat: added vote test cases

---
+++ tests/Feature/VoteShowPageTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Livewire\IdeaShow;
+use App\Models\Category;
+use App\Models\Idea;
+use App\Models\Status;
+use App\Models\User;
+use App\Models\Vote;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class VoteShowPageTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function show_page_has_correct_votes_count()
+    {
+        $user = User::factory()->create();
+        $userB = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea',
+        ]);
+
+        Vote::factory()->create([
+            'idea_id' => $idea->id,
+            'user_id' => $user->id,
+        ]);
+
+        Vote::factory()->create([
+            'idea_id' => $idea->id,
+            'user_id' => $userB->id,
+        ]);
+
+        $this->get(route('idea.show', $idea))
+            ->assertViewHas('votesCount', 2);
+    }
+}
