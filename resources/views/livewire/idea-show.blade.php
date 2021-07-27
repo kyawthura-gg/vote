2f915495a3ed074919eb98199a55e82706554688
kyawthura-gg <ggaryal@gmail.com>
2021-07-27 16:22:56 +0630

    fix: idea show view

--- resources/views/livewire/idea-show.blade.php
+++ resources/views/livewire/idea-show.blade.php
@@ -39,12 +39,18 @@
 
                     <div class="flex items-center mt-4 md:hidden md:mt-0">
                         <div class="h-10 px-4 py-2 pr-8 text-center bg-gray-100 rounded-xl">
-                            <div class="text-sm font-bold leading-none">{{ $votesCount }}</div>
+                            <div class="text-sm font-bold leading-none @if($hasVoted) text-blue @endif">{{ $votesCount }}</div>
                             <div class="font-semibold leading-none text-gray-400 text-xxs">Votes</div>
                         </div>
+                        @if ($hasVoted)
+                        <button class="w-20 px-4 py-3 -mx-5 font-bold text-white uppercase transition duration-150 ease-in border bg-blue border-blue text-xxs rounded-xl hover:bg-blue-hover">
+                            Voted
+                        </button>
+                        @else
                         <button class="w-20 px-4 py-3 -mx-5 font-bold uppercase transition duration-150 ease-in bg-gray-200 border border-gray-200 text-xxs rounded-xl hover:border-gray-400">
                             Vote
                         </button>
+                        @endif
                     </div>
                 </div>
             </div>
@@ -149,12 +155,18 @@
 
         <div class="items-center hidden space-x-3 md:flex">
             <div class="px-3 py-2 font-semibold text-center bg-white rounded-xl">
-                <div class="text-xl leading-snug">{{ $votesCount }}</div>
+                <div class="text-xl leading-snug @if($hasVoted) text-blue @endif">{{ $votesCount }}</div>
                 <div class="text-xs leading-none text-gray-400">Votes</div>
             </div>
+            @if ($hasVoted)
+            <button type="button" class="w-32 px-6 py-3 text-xs font-semibold text-white uppercase transition duration-150 ease-in border h-11 bg-blue rounded-xl border-blue hover:bg-blue-hover">
+                <span>Voted</span>
+            </button>
+            @else
             <button type="button" class="w-32 px-6 py-3 text-xs font-semibold uppercase transition duration-150 ease-in bg-gray-200 border border-gray-200 h-11 rounded-xl hover:border-gray-400">
                 <span>Vote</span>
             </button>
+            @endif
         </div>
     </div> <!-- end buttons-container -->
 </div>
