11f1c8e46dd512dba9e8988f3608be0dab54343e
kyawthura-gg <ggaryal@gmail.com>
2021-07-27 16:22:10 +0630

    fix: idea livewire

--- app/Http/Livewire/IdeaIndex.php
+++ app/Http/Livewire/IdeaIndex.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Idea;
 use Livewire\Component;
 
 class IdeaIndex extends Component
 {
     public $idea;
     public $votesCount;
+    public $hasVoted;
+
+    public function mount(Idea $idea, $votesCount)
+    {
+        $this->idea = $idea;
+        $this->votesCount = $votesCount;
+        $this->hasVoted = $idea->voted_by_user;
+    }
 
     public function render()
     {
--- app/Http/Livewire/IdeaShow.php
+++ app/Http/Livewire/IdeaShow.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Idea;
 use Livewire\Component;
 
 class IdeaShow extends Component
 {
     public $idea;
     public $votesCount;
+    public $hasVoted;
+
+    public function mount(Idea $idea, $votesCount)
+    {
+        $this->idea = $idea;
+        $this->votesCount = $votesCount;
+        $this->hasVoted = $idea->isVotedByUser(auth()->user());
+    }
 
     public function render()
     {
