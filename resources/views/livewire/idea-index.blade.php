ca60f1ce44ee133c33fe507825bdd7fe906bafaa
kyawthura-gg <ggaryal@gmail.com>
2021-07-28 11:13:15 +0630

    fix: add livewire click method in index view

--- resources/views/livewire/idea-index.blade.php
+++ resources/views/livewire/idea-index.blade.php
@@ -14,9 +14,9 @@
 
         <div class="mt-8">
             @if ($hasVoted)
-            <button class="w-20 px-4 py-3 font-bold text-white uppercase transition duration-150 ease-in border bg-blue border-blue hover:bg-blue-hover text-xxs rounded-xl">Voted</button>
+            <button wire:click.prevent="vote" class="w-20 px-4 py-3 font-bold text-white uppercase transition duration-150 ease-in border bg-blue border-blue hover:bg-blue-hover text-xxs rounded-xl">Voted</button>
             @else
-            <button class="w-20 px-4 py-3 font-bold uppercase transition duration-150 ease-in bg-gray-200 border border-gray-200 hover:border-gray-400 text-xxs rounded-xl">Vote</button>
+            <button wire:click.prevent="vote" class="w-20 px-4 py-3 font-bold uppercase transition duration-150 ease-in bg-gray-200 border border-gray-200 hover:border-gray-400 text-xxs rounded-xl">Vote</button>
             @endif
         </div>
     </div>
@@ -61,11 +61,11 @@
                         <div class="font-semibold leading-none text-gray-400 text-xxs">Votes</div>
                     </div>
                     @if ($hasVoted)
-                    <button class="w-20 px-4 py-3 -mx-5 font-bold text-white uppercase transition duration-150 ease-in border bg-blue border-blue text-xxs rounded-xl hover:bg-blue-hover">
+                    <button wire:click.prevent="vote" class="w-20 px-4 py-3 -mx-5 font-bold text-white uppercase transition duration-150 ease-in border bg-blue border-blue text-xxs rounded-xl hover:bg-blue-hover">
                         Voted
                     </button>
                     @else
-                    <button class="w-20 px-4 py-3 -mx-5 font-bold uppercase transition duration-150 ease-in bg-gray-200 border border-gray-200 text-xxs rounded-xl hover:border-gray-400">
+                    <button wire:click.prevent="vote" class="w-20 px-4 py-3 -mx-5 font-bold uppercase transition duration-150 ease-in bg-gray-200 border border-gray-200 text-xxs rounded-xl hover:border-gray-400">
                         Vote
                     </button>
                     @endif
