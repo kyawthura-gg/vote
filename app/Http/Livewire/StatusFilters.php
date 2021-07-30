e1e6bb9a498ee4a588cd025e4ce4805fbfc40187
kyawthura-gg <ggaryal@gmail.com>
2021-07-30 17:14:13 +0630

    feat: added status filter class

---
+++ app/Http/Livewire/StatusFilters.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Status;
+use Illuminate\Support\Facades\Route;
+use Livewire\Component;
+
+class StatusFilters extends Component
+{
+    public $status;
+    public $statusCount;
+
+    protected $queryString = [
+        'status',
+    ];
+
+    public function mount()
+    {
+        $this->statusCount = Status::getCount();
+        $this->status = request()->status ?? 'All';
+
+        if (Route::currentRouteName() === 'idea.show') {
+            $this->status = null;
+        }
+    }
+
+    public function setStatus($newStatus)
+    {
+        $this->status = $newStatus;
+
+        if ($this->getPreviousRouteName() === 'idea.show') {
+            return redirect()->route('idea.index', [
+                'status' => $this->status,
+            ]);
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.status-filters');
+    }
+
+    public function getPreviousRouteName()
+    {
+        return app('router')->getRoutes()->match(app('request')->create(url()->previous()))->getName();
+    }
+}
