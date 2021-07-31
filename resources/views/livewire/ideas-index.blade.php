3bd9b4416619d6c34e9bc6a652f5fcaf31100e19
kyawthura-gg <ggaryal@gmail.com>
2021-07-31 16:32:18 +0630

    feat: added empty ideas div

--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -9,15 +9,14 @@
             </select>
         </div>
         <div class="w-full md:w-1/3">
-            <select name="other_filters" id="other_filters" class="w-full px-4 py-2 border-none rounded-xl">
-                <option value="Filter One">Filter One</option>
-                <option value="Filter Two">Filter Two</option>
-                <option value="Filter Three">Filter Three</option>
-                <option value="Filter Four">Filter Four</option>
+            <select wire:model="filter" name="other_filters" id="other_filters" class="w-full px-4 py-2 border-none rounded-xl">
+                <option value="No Filter">No Filter</option>
+                <option value="Top Voted">Top Voted</option>
+                <option value="My Ideas">My Ideas</option>
             </select>
         </div>
         <div class="relative w-full md:w-2/3">
-            <input type="search" placeholder="Find an idea" class="w-full px-4 py-2 pl-8 placeholder-gray-900 bg-white border-none rounded-xl">
+            <input wire:model="search" type="search" placeholder="Find an idea" class="w-full px-4 py-2 pl-8 placeholder-gray-900 bg-white border-none rounded-xl">
             <div class="absolute top-0 flex h-full ml-2 itmes-center">
                 <svg class="w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -27,11 +26,18 @@
     </div> <!-- end filters -->
 
     <div class="my-8 space-y-6 ideas-container">
-        @foreach ($ideas as $idea)
+        @forelse ($ideas as $idea)
         <livewire:idea-index :key="$idea->id" :idea="$idea" :votesCount="$idea->votes_count" />
-        @endforeach
+        @empty
+        <div class="mx-auto mt-12 w-70">
+            <img src="{{ asset('img/no-idea.svg') }}" alt="No Ideas" class="mx-auto" style="mix-blend-mode: luminosity">
+            <div class="mt-6 font-bold text-center text-gray-400">No ideas were found...</div>
+        </div>
+        @endforelse
     </div> <!-- end ideas-container -->
+
     <div class="my-8">
+        {{-- {{ $ideas->links() }} --}}
         {{ $ideas->appends(request()->query())->links() }}
     </div>
 </div>
