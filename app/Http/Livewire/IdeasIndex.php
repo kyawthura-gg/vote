b88d46f58283479e5287d28e13c1657fdef1f77e
kyawthura-gg <ggaryal@gmail.com>
2021-07-31 16:32:58 +0630

    fix: added update and where in idea index

--- app/Http/Livewire/IdeasIndex.php
+++ app/Http/Livewire/IdeasIndex.php
@@ -15,10 +15,14 @@ class IdeasIndex extends Component
 
     public $status;
     public $category;
+    public $filter;
+    public $search;
 
     protected $queryString = [
         'status',
         'category',
+        'filter',
+        'search',
     ];
 
     protected $listeners = ['queryStringUpdatedStatus'];
@@ -31,6 +35,25 @@ class IdeasIndex extends Component
     public function updatingCategory()
     {
         $this->resetPage();
+    }
+
+    public function updatingFilter()
+    {
+        $this->resetPage();
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+    public function updatedFilter()
+    {
+        if ($this->filter === 'My Ideas') {
+            if (!auth()->check()) {
+                return redirect()->route('login');
+            }
+        }
     }
 
     public function queryStringUpdatedStatus($newStatus)
@@ -50,6 +73,12 @@ class IdeasIndex extends Component
                     return $query->where('status_id', $statuses->get($this->status));
                 })->when($this->category && $this->category !== 'All Categories', function ($query) use ($categories) {
                     return $query->where('category_id', $categories->pluck('id', 'name')->get($this->category));
+                })->when($this->filter && $this->filter === 'Top Voted', function ($query) {
+                    return $query->orderByDesc('votes_count');
+                })->when($this->filter && $this->filter === 'My Ideas', function ($query) {
+                    return $query->where('user_id', auth()->id());
+                })->when(strlen($this->search) >= 3, function ($query) {
+                    return $query->where('title', 'like', '%' . $this->search . '%');
                 })
                 ->addSelect([
                     'voted_by_user' => Vote::select('id')
