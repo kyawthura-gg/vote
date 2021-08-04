153797a0be1d3cb2b265163ee05e90ad3d24a406
kyawthura-gg <ggaryal@gmail.com>
2021-08-04 21:56:29 +0630

    fix: added message for notification

--- app/Http/Livewire/CreateIdea.php
+++ app/Http/Livewire/CreateIdea.php
@@ -41,7 +41,7 @@ class CreateIdea extends Component
 
         $idea->vote(auth()->user());
 
-        session()->flash('success_message', 'Idea was added successfully.');
+        session()->flash('success_message', 'Idea was added successfully!');
 
         $this->reset();
 
