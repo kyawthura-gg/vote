c905ea4901d443974acbfce9e638c36a6b13f091
kyawthura-gg <ggaryal@gmail.com>
2021-08-06 20:57:31 +0630

    feat: added comment update listener in ideas comment

--- app/Http/Livewire/IdeaComments.php
+++ app/Http/Livewire/IdeaComments.php
@@ -2,18 +2,29 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Comment;
 use App\Models\Idea;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class IdeaComments extends Component
 {
+    use WithPagination;
+
     public $idea;
 
-    protected $listeners = ['commentWasAdded'];
+    protected $listeners = ['commentWasAdded', 'commentWasDeleted'];
 
     public function commentWasAdded()
     {
         $this->idea->refresh();
+        $this->goToPage($this->idea->comments()->paginate()->lastPage());
+    }
+
+    public function commentWasDeleted()
+    {
+        $this->idea->refresh();
+        $this->goToPage(1);
     }
 
     public function mount(Idea $idea)
@@ -24,7 +35,8 @@ class IdeaComments extends Component
     public function render()
     {
         return view('livewire.idea-comments', [
-            'comments' => $this->idea->comments,
+            // 'comments' => $this->idea->comments()->paginate()->withQueryString(),
+            'comments' => Comment::with('user')->where('idea_id', $this->idea->id)->paginate()->withQueryString(),
         ]);
     }
 }
