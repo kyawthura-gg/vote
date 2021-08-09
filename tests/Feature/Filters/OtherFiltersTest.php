467fc0f3c37bf89865d0a34c69e8e6c8557ebe02
kyawthura-gg <ggaryal@gmail.com>
2021-08-09 10:27:17 +0630

    test: added spam comment filter test

--- tests/Feature/Filters/OtherFiltersTest.php
+++ tests/Feature/Filters/OtherFiltersTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Filters;
 
 use App\Http\Livewire\IdeasIndex;
 use App\Models\Category;
+use App\Models\Comment;
 use App\Models\Idea;
 use App\Models\User;
 use App\Models\Vote;
@@ -202,4 +203,41 @@ class OtherFiltersTest extends TestCase
                     && $ideas->get(2)->title === 'Idea One';
             });
     }
+
+     /** @test */
+     public function spam_comments_filter_works()
+     {
+         $user = User::factory()->admin()->create();
+
+         $ideaOne = Idea::factory()->create([
+             'title' => 'Idea One',
+         ]);
+
+         $ideaTwo = Idea::factory()->create([
+             'title' => 'Idea Two',
+         ]);
+
+         $ideaThree = Idea::factory()->create([
+             'title' => 'Idea Two',
+         ]);
+
+         $commentOne = Comment::factory()->create([
+             'idea_id' => $ideaOne->id,
+             'body' => 'This is my first comment',
+             'spam_reports' => 3,
+         ]);
+
+         $commentTwo = Comment::factory()->create([
+             'idea_id' => $ideaTwo->id,
+             'body' => 'This is my second comment',
+             'spam_reports' => 2,
+         ]);
+
+         Livewire::actingAs($user)
+             ->test(IdeasIndex::class)
+             ->set('filter', 'Spam Comments')
+             ->assertViewHas('ideas', function ($ideas) {
+                 return $ideas->count() === 2;
+             });
+     }
 }
